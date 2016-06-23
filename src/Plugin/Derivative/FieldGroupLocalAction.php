60354310bdffd79f4ee4b743c59a831a9b390a29
webflo <webflo@254778.no-reply.drupal.org>
2016-06-23 15:06:32 +0200

    Issue #2719569 by webflo, jalpesh: Missing dependency with field_ui

--- src/Plugin/Derivative/FieldGroupLocalAction.php
+++ src/Plugin/Derivative/FieldGroupLocalAction.php
@@ -1,28 +1,65 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\field_group\Plugin\Derivative\FieldGroupLocalAction.
- */
-
 namespace Drupal\field_group\Plugin\Derivative;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
-use Drupal\field_ui\Plugin\Derivative\FieldUiLocalAction;
+use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Component\Plugin\Derivative\DeriverBase;
 
 /**
  * Provides local action definitions for all entity bundles.
  */
-class FieldGroupLocalAction extends FieldUiLocalAction implements ContainerDeriverInterface {
+class FieldGroupLocalAction extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The route provider.
+   *
+   * @var \Drupal\Core\Routing\RouteProviderInterface
+   */
+  protected $routeProvider;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a FieldUiLocalAction object.
+   *
+   * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
+   *   The route provider to load routes by name.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(RouteProviderInterface $route_provider, EntityTypeManagerInterface $entity_type_manager) {
+    $this->routeProvider = $route_provider;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('router.route_provider'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
-
     $this->derivatives = array();
 
-    foreach ($this->entityManager->getDefinitions() as $entity_type_id => $entity_type) {
+    foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
       if ($entity_type->get('field_ui_base_route')) {
 
         $default_options = [
