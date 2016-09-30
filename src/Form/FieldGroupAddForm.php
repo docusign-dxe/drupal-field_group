c2c3e3f95ba187dd59a4050be477a62c4002d43c
zuuperman <nils.destoop@gmail.com>
2016-09-30 13:43:09 +0100

    Issue #2783089: Doesn't save label settings in field widget settings

--- src/Form/FieldGroupAddForm.php
+++ src/Form/FieldGroupAddForm.php
@@ -245,11 +245,12 @@ class FieldGroupAddForm extends FormBase {
         'children' =>[],
         'parent_name' => '',
         'weight' => 20,
-        'label' => $form_state->get('group_label'),
         'format_type' => $form_state->get('group_formatter'),
       );
 
       $new_group->format_settings = $form_state->getValue('format_settings');
+      $new_group->label = $new_group->format_settings['label'];
+      unset($new_group->format_settings['label']);
       $new_group->format_settings += _field_group_get_default_formatter_settings($form_state->get('group_formatter'), $this->context);
 
       field_group_group_save($new_group);
