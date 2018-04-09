97e3562401b156639ba6fd89a46cd450aeaefa4e
web247 <web247@176346.no-reply.drupal.org>
2018-04-10 00:05:58 +0200

    Issue #2921228 by web247: Accordion doesn't work

--- src/Element/Accordion.php
+++ src/Element/Accordion.php
@@ -42,6 +42,7 @@ class Accordion extends RenderElement {
 
     // Add the jQuery UI accordion.
     $element['#attached']['library'][] = 'field_group/formatter.accordion';
+    $element['#attached']['library'][] = 'field_group/core';
 
     // Add the effect class.
     if (isset($element['#effect'])) {
